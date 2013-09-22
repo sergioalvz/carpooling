1d86b9047940f001f8deb82d512f995c6ac3322e
nataliagmlo <nataliagmlo@gmail.com>
2013-09-22 23:26:41 +0200

    ShowRouteDetailsAction

--- src/org/miw/sig/carpooling/presentation/ShowRouteDetailsAction.java
+++ src/org/miw/sig/carpooling/presentation/ShowRouteDetailsAction.java
@@ -3,30 +3,48 @@ package org.miw.sig.carpooling.presentation;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.miw.sig.carpooling.model.Marker;
 import org.miw.sig.carpooling.model.Route;
 import org.miw.sig.carpooling.persistence.CarpoolingDAO;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ShowRouteDetailsAction extends ActionSupport implements ServletRequestAware{
+public class ShowRouteDetailsAction extends ActionSupport implements
+		ServletRequestAware {
 
 	private static final long serialVersionUID = 8814132631547577129L;
 	private Integer routeId;
 	private HttpServletRequest request;
-	
-	public void setRouteId(Integer routeId){
+
+	public void setRouteId(Integer routeId) {
 		this.routeId = routeId;
 	}
 
 	@Override
 	public String execute() throws Exception {
 		Route route = new CarpoolingDAO().getRoute(routeId);
-		//TODO generate the String 'email@@@......'
+		String info = route.getEmail() + "@@@from@@"
+				+ route.getFrom().getName() + "@@"
+				+ route.getFrom().getLatitude() + "@@"
+				+ route.getFrom().getLongitude() + "@@@to@@"
+				+ route.getTo().getName() + "@@" + route.getTo().getLatitude()
+				+ "@@" + route.getTo().getLongitude();
+
+		if (route.getMarkers() != null && route.getMarkers().size() > 0) {
+			for (Marker marker : route.getMarkers()) {
+				info += "@@@waypoint@@" + marker.getName() + "@@"
+						+ marker.getLatitude() + "@@" + marker.getLongitude();
+			}
+
+		}
+
+		request.setAttribute("savedRoute", info);
 		return ActionSupport.SUCCESS;
 	}
 
 	@Override
 	public void setServletRequest(HttpServletRequest request) {
-		this.request = request;		
-	}
+		this.request = request;
+	}	
+	
 }
